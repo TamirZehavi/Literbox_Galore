15b8400e2a63004a49d6271404a2c73388c80b10
EllaLuna <64199423+EllaLuna@users.noreply.github.com>
2021-01-05 16:47:41 +0200

    Everything is a prefab now

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        cameraOffset = new Vector3(0, 50, -10);
+        cameraOffset = new Vector3(0, 60, -30);
     }
 
     // Update is called once per frame
